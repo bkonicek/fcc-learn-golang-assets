e65e65289f32f768fa75795d1b4d6c2aa2244f6b
Ben Konicek <ben.konicek@gmail.com>
2024-07-14 16:30:41 -0400

    More loop solutions

--- course/7-loops/exercises/5-continue_and_break/code.go
+++ course/7-loops/exercises/5-continue_and_break/code.go
@@ -2,10 +2,31 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func printPrimes(max int) {
-	// ?
+	for i := 2; i <= max; i++ {
+		if i == 2 {
+			fmt.Println(i)
+			continue
+		}
+		if i%2 == 0 {
+			continue
+		}
+
+		isPrime := true
+		for x := 3; x <= int(math.Sqrt(float64(i))); x++ {
+			if i%x == 0 {
+				isPrime = false
+				break
+			}
+		}
+		if isPrime {
+			fmt.Println(i)
+		}
+		i++
+	}
 }
 
 // don't edit below this line
