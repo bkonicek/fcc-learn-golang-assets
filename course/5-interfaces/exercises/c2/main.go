70c36646f2af25aadd4bf06e2d5d7d9aadf3c670
Ben Konicek <ben.konicek@gmail.com>
2024-07-14 14:42:38 -0400

    5-interfaces/c2: completed solution

--- course/5-interfaces/exercises/c2/main.go
+++ course/5-interfaces/exercises/c2/main.go
@@ -11,10 +11,22 @@ type directMessage struct {
 	isUrgent       bool
 }
 
+func (dM directMessage) importance() int {
+	importance := dM.priorityLevel
+	if dM.isUrgent {
+		importance = 50
+	}
+	return importance
+}
+
 type groupMessage struct {
 	groupName      string
 	messageContent string
 	priorityLevel  int
+}
+
+func (gM groupMessage) importance() int {
+	return gM.priorityLevel
 }
 
 type systemAlert struct {
@@ -22,8 +34,19 @@ type systemAlert struct {
 	messageContent string
 }
 
-// ?
+func (sA systemAlert) importance() int {
+	return 100
+}
 
 func processNotification(n notification) (string, int) {
-	// ?
+	switch v := n.(type) {
+	case directMessage:
+		return v.senderUsername, v.importance()
+	case groupMessage:
+		return v.groupName, v.importance()
+	case systemAlert:
+		return v.alertCode, v.importance()
+	default:
+		return "", 0
+	}
 }
