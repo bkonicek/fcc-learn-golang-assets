89fc5d792a8c47d8a2c4e915bf7ec49dd2ce1faf
Ben Konicek <ben.konicek@gmail.com>
2024-07-14 22:55:29 -0400

    8-04: solution

--- course/8-slices/exercises/4-slices_no_array/code.go
+++ course/8-slices/exercises/4-slices_no_array/code.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func getMessageCosts(messages []string) []float64 {
-	// ?
+	costs := make([]float64, len(messages))
+	for i, m := range messages {
+		costs[i] = float64(len(m)) * 0.01
+	}
+	return costs
 }
 
 // don't edit below this line
